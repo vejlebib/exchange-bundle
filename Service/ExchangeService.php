e54933a09b266c90b720a4c04e27b251d15606be
Troels Ugilt Jensen <tujensen@gmail.com>
2017-10-10 10:40:01 +0200

    OS2DISP-113: Fixed feed import

--- Service/ExchangeService.php
+++ Service/ExchangeService.php
@@ -7,7 +7,7 @@
 namespace Os2Display\ExchangeBundle\Service;
 
 use Doctrine\ORM\EntityManager;
-use Indholdskanalen\MainBundle\Events\CronEvent;
+use Os2Display\CoreBundle\Events\CronEvent;
 
 /**
  * Class ExchangeService
@@ -18,6 +18,7 @@ class ExchangeService
     protected $exchangeWebService;
     protected $serviceEnabled;
     protected $entityManager;
+    protected $resourceMail;
 
     /**
      * ExchangeService constructor.
@@ -26,11 +27,16 @@ class ExchangeService
      * @param EntityManager $entityManager
      * @param $serviceEnabled
      */
-    public function __construct(ExchangeWebService $exchangeWebService, EntityManager $entityManager, $serviceEnabled)
-    {
+    public function __construct(
+        ExchangeWebService $exchangeWebService,
+        EntityManager $entityManager,
+        $serviceEnabled,
+        $resourceMail
+    ) {
         $this->exchangeWebService = $exchangeWebService;
         $this->serviceEnabled = $serviceEnabled;
         $this->entityManager = $entityManager;
+        $this->resourceMail = $resourceMail;
     }
 
     /**
@@ -58,8 +64,11 @@ class ExchangeService
      * @return array
      *   Array of ExchangeBookings.
      */
-    public function getExchangeBookingsForInterval($resourceMail, $startTime, $endTime)
-    {
+    public function getExchangeBookingsForInterval(
+        $resourceMail,
+        $startTime,
+        $endTime
+    ) {
         // Start by getting the bookings from exchange.
         $calendar = $this->exchangeWebService->getResourceBookings($resourceMail, $startTime, $endTime);
 
@@ -78,52 +87,38 @@ class ExchangeService
 
         // For each calendar slide
         $slides = $this->entityManager
-            ->getRepository('IndholdskanalenMainBundle:Slide')->findBySlideType('calendar');
+            ->getRepository('Os2DisplayCoreBundle:Slide')
+            ->findBySlideType('calendar');
+
         $todayStart = time() - 3600;
         // Round down to nearest hour
         $todayStart = $todayStart - ($todayStart % 3600);
 
-        $todayEnd = mktime(23, 59, 59);
+        $end = strtotime('+7 days', mktime(23, 59, 29));
+
+        $bookings = [];
+
+        try {
+            $resourceBookings = $this->getExchangeBookingsForInterval($this->resourceMail, $todayStart, $end);
+
+            if (count($resourceBookings) > 0) {
+                $bookings = array_merge($bookings, $resourceBookings);
+            }
+        } catch (\Exception $e) {
+            // Ignore exceptions. The show must keep running, even though we have no connection to exchange.
+        }
+
+        // Sort bookings by start time.
+        usort($bookings, function ($a, $b) {
+            return strcmp($a->getStartTime(), $b->getStartTime());
+        });
 
         // Get data for interest period
         foreach ($slides as $slide) {
-            $bookings = [];
-
-            $options = $slide->getOptions();
-
-            foreach ($options['resources'] as $resource) {
-                $interestInterval = 0;
-                // Read interestInterval from options.
-                if (isset($options['interest_interval'])) {
-                    $interestInterval = $options['interest_interval'];
-                }
-                $interestInterval = max(0, $interestInterval - 1) + 28;
-
-                // Move today with number of requested days.
-                $end = strtotime('+' . $interestInterval . ' days', $todayEnd);
-
-                try {
-                    $resourceBookings = $this->getExchangeBookingsForInterval($resource['mail'], $todayStart, $end);
-
-                    if (count($resourceBookings) > 0) {
-                        $bookings = array_merge($bookings, $resourceBookings);
-                    }
-                } catch (\Exception $e) {
-                    // Ignore exceptions. The show must keep running, even though we have no connection to koba.
-                }
-            }
-
-
-            // Sort bookings by start time.
-            usort($bookings, function ($a, $b) {
-                return strcmp($a->getStartTime(), $b->getStartTime());
-            });
-
-
             // Save in externalData field
             $slide->setExternalData($bookings);
-
-            $this->entityManager->flush();
         }
+
+        $this->entityManager->flush();
     }
 }
